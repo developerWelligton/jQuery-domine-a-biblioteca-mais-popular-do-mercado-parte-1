1fd333e4a02d0f56b9168d6530b145e802d7ee8c
welligton_fadami <welligtoncos@gmail.com>
2022-11-20 18:17:50 -0300

    Criando um botão remover

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -38,9 +38,7 @@ function inicializaCronometro(){
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
             if(tempoRestante < 1){
-                clearInterval(cronometroID);
-                campo.attr("disabled",true); 
-                campo.addClass("campo-desativado")
+                clearInterval(cronometroID); 
                 finalizaJogo();
             }  
         }, 1000);
@@ -74,14 +72,19 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Welligton";
     var numPalavras = $("#contador-palavras").text();
+    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
     var linha = "<tr>" +
                     "<td>" + usuario + "</td>"+
                     "<td>" + numPalavras + "</td>"+
+                    "<td>" + botaoRemover + "</td>"+
                 "</tr>";
 
     corpoTabela.prepend(linha)
 }
- 
+$(".botao-remover").click(function(event){
+    event.preventDefault();
+    $(this).parent().parent().remove();
+});
 function reiniciaJogo(){
     campo.attr("disabled",false);
     campo.val("");
