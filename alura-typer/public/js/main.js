0da1a7d38a67f121163077b8895c0eac0091b352
welligton_fadami <welligtoncos@gmail.com>
2022-11-21 21:21:28 -0300

    Organizando o código da aplicação

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -67,41 +67,7 @@ function inicializaMarcadores(){
            campo.removeClass("borda-verde");
        }
     });
-}
-function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Welligton";
-    var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
-    var linha = novaLinha(usuario,numPalavras);
-    linha.find(".botao-remover").click(removeLinha);
-
-    corpoTabela.append(linha);
-}
-function novaLinha(usuario, palavras){
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>");
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href","#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-    // Icone dentro do <a>
-    link.append(icone);
-
-    // <a> dentro do <td>
-    colunaRemover.append(link);
-
-    // Os três <td> dentro do <tr>
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-    return linha;
-}
-function removeLinha(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
+} 
 function reiniciaJogo(){
     campo.attr("disabled",false);
     campo.val("");
