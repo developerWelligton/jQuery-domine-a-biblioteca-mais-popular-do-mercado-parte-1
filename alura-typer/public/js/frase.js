4397ac1593de7b9dc7ed7e7f2810469baa2adc24
welligton_fadami <welligtoncos@gmail.com>
2022-11-27 13:10:14 -0300

    Buscando dados com get

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria); 
+$("#botao-frase-id").click(buscaFrase); 
 
 function fraseAleatoria(){ 
     $("#spinner").show();
@@ -22,11 +23,50 @@ function fraseAleatoria(){
         $("#spinner").hide();
     })
 }
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val(); 
+    var dados = { id: fraseId}
+
+   
+    $.get({
+        url: 'http://localhost:3000/frases',
+        contentType: 'application/json',
+        cache: false,
+        method: 'GET',
+        dataType: 'json',
+        data:dados,
+        success: trocaFrase
+    }).fail(
+        function(){
+            $("#naoEncontrado").toggle();
+            setTimeout(function(){
+                $("#naoEncontrado").toggle();
+            },2500);
+             
+        }
+    ).always(function(){
+        $("#spinner").toggle();
+    })
+}
+
+function trocaFrase(data){ 
+    if(data.length > 1){ 
+        $("#EncontradoNaoEspecificado").show(); 
+    }else{
+        $("#EncontradoNaoEspecificado").hide();
+        var frase = $(".frase");
+        frase.text(data.texto);
+        atualizaTamanhoFrase();
+        atualizaTempoInicial(data.tempo); 
+    } 
+}
  
 
 
 function trocaFraseAleatoria(data){
-    var frase = $(".frase");
+ 
     var numeroAleatorio = Math.floor(Math.random() * data.length)
     frase.text(data[numeroAleatorio].texto)
     atualizaTamanhoFrase();
