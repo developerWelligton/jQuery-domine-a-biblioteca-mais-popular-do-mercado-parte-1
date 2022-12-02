16a8fcbcbde0f41c9da5e48deba6252acdeafa89
welligton_fadami <welligtoncos@gmail.com>
2022-12-02 15:25:30 -0300

    mask km/metro funcionando

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,19 +1,5 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-//li
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numPalavras);
-
-
-var campo = $(".campo-digitacao")
-campo.on("input", function(){
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    var qtdCaracteres = conteudo.length;
-    $("#contador-palavras").text(qtdPalavras)
-    $("#contador-caracteres").text(qtdCaracteres);
-})
-console.log(campo)
+ $(document).ready(function(){
+    $("#km").mask("000+000",{reverse:true})
+ })
+ 
  
